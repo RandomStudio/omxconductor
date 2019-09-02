abe713e385ce0b848a96b262e214b85466d541f2
Stephen Buchanan <stephen@pop-os.localdomain>
2019-09-02 16:27:16 +0200

    feat: default audio output option

--- src/__tests__/player.spec.ts
+++ src/__tests__/player.spec.ts
@@ -5,8 +5,13 @@ jest.mock('../environment.ts', () => ({
 
 import { Player } from '../player'
 
-describe(`Player`, () => {
-  const player = new Player({})
+describe(`Player defaults`, () => {
+  let player: Player
+
+  beforeEach(() => {
+    player = new Player({})
+  })
+
   it('should return an object', () => {
     expect(typeof player).toBe('object')
   })
@@ -16,22 +21,7 @@ describe(`Player`, () => {
   it('should get default dBUS ID if none specified', () => {
     expect(player.getSettings().dBusId).toBe('org.mpris.MediaPlayer2.omxplayer')
   })
-  // let greeter: Greeter
-  // beforeEach(() => {
-  //   greeter = new Greeter('World')
-  // })
-  // it(`should greet`, () => {
-  //   const actual = greeter.greet()
-  //   const expected = 'Hello, World!'
-  //   expect(actual).toBe(expected)
-  // })
-  // it(`should greet and print deprecation message if in dev mode`, () => {
-  //   const spyWarn = jest.spyOn(console, 'warn')
-  //   const actual = greeter.greetMe()
-  //   const expected = 'Hello, World!'
-  //   expect(actual).toBe(expected)
-  //   expect(spyWarn).toHaveBeenCalledWith(
-  //     'this method is deprecated, use #greet instead'
-  //   )
-  // })
+  it('should get default audio output option', () => {
+    expect(player.getSettings().audioOutput === 'both')
+  })
 })
--- src/player.ts
+++ src/player.ts
@@ -1,17 +1,26 @@
 // import { IS_DEV } from './environment'
 import * as defaults from './defaults'
+
+enum AudioOutput {
+  hdmi = 'hdmi',
+  local = 'local',
+  both = 'both',
+}
+
 interface PlayerOptions {
   layer: number | null
   dBusId: string
+  audioOutput: AudioOutput
 }
 
 export class Player {
   private settings: PlayerOptions
-  constructor({ layer = 1, dBusId = defaults.DBUS_DEST_DEFAULT }) {
-    this.settings = {
-      layer,
-      dBusId,
-    }
+  constructor({
+    layer = 1,
+    dBusId = defaults.DBUS_DEST_DEFAULT,
+    audioOutput = AudioOutput.both,
+  }) {
+    this.settings = { layer, dBusId, audioOutput }
   }
 
   getSettings() {
